1f35d5d95a7cdcd0c2879967f2d0ff2ce6a00204
Cora Iberkleid <ciberkleid@gmail.com>
2021-08-31 23:07:05 -0400

    initial commit (new prj, web & testcontainers dependencies)

---
+++ src/main/java/com/example/testcontainersdemo/TestcontainersDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.testcontainersdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainersDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontainersDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/testcontainersdemo/TestcontainersDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testcontainersdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainersDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
