929a67d4cf64c6084d81da3b5b484ee8562b1a8f
Maria Gabriella Brodi <mbrodi@pivotal.io>
2021-09-02 11:21:25 -0400

    Cleanup

--- src/test/resources/db/demo1/V002__interval.sql
+++ src/test/resources/db/demo1/V002__interval.sql
@@ -1,10 +1,6 @@
 insert into messages (id, username, text)
 values (nextval('hibernate_sequence'), 'Dani_Rojas', 'Football is life!');
 
-create table if not exists old_messages AS select * from messages;
-
-select * from old_messages;
-
 alter table messages
     add column date_of_expiration date;
 
